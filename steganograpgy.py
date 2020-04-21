537cb896998fab1fb52d8911d159d69cfcac594f
Ben <benlaugherty@gmail.com>
2020-04-21 17:44:27 -0600

    working decoder and decoded image :)

--- steganograpgy.py
+++ steganograpgy.py
@@ -1,3 +1,5 @@
+from PIL import Image, ImageFont, ImageDraw
+
 def decode_image(file_location):
     encoded_image = Image.open(file_location)
     red_channel = encoded_image.split()[0]
@@ -8,7 +10,18 @@ def decode_image(file_location):
     decoded_image = Image.new("RGB", encoded_image.size)
     pixels = decoded_image.load()
 
-    #TODO: Fill in decoding functionality
+    # loop through all pixels
+    for i in range(x_size):
+        for j in range(y_size):
+            # check to see if current pixel's red value is 0, and write to to black
+            if bin(red_channel.getpixel((i, j)))[-1] == '0':
+                # write black on images with 'no red'
+                pixels[i, j] = (255, 255, 255)
+            else:
+                # write write to everything else to actually show the message
+                pixels[i, j] = (0, 0, 0)
 
     decoded_image.save("images/decoded_image.png")
 
+if __name__ == '__main__':
+    decode_image('images/encoded_sample.png')
